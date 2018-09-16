a848e8f2f0f85d81605864b28e5e11d23fcff343
yu.xiang <yu.xiang@alibaba-inc.com>
2018-09-16 12:18:42 +0800

    add_miniprogram_and_jwt

--- config/index.js
+++ config/index.js
@@ -5,5 +5,8 @@ const { env } = process;
 module.exports = {
   host: env.HOST,
   port: env.PORT,
-  jwtSecret: env.JWT_SECRET
+  jwtSecret: env.JWT_SECRET,
+  AppID: env.WXAppID,
+  AppSecret: env.WXAppSecret,
+
 }
